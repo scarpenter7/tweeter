c3b8493011250c2018757b984d2d19e628b6a01d
scarpenter7 <scarpenter7@outlook.com>
2023-02-15 23:03:32 -0700

    unduped IsFollowerTask

--- app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/IsFollowerTask.java
+++ app/src/main/java/edu/byu/cs/tweeter/client/model/service/backgroundTask/IsFollowerTask.java
@@ -13,18 +13,9 @@ import edu.byu.cs.tweeter.model.domain.User;
 /**
  * Background task that determines if one user is following another.
  */
-public class IsFollowerTask implements Runnable {
-    private static final String LOG_TAG = "IsFollowerTask";
-
-    public static final String SUCCESS_KEY = "success";
+public class IsFollowerTask extends AuthenticatedTask {
     public static final String IS_FOLLOWER_KEY = "is-follower";
-    public static final String MESSAGE_KEY = "message";
-    public static final String EXCEPTION_KEY = "exception";
 
-    /**
-     * Auth token for logged-in user.
-     */
-    private AuthToken authToken;
     /**
      * The alleged follower.
      */
@@ -33,60 +24,26 @@ public class IsFollowerTask implements Runnable {
      * The alleged followee.
      */
     private User followee;
-    /**
-     * Message handler that will receive task results.
-     */
-    private Handler messageHandler;
+
+    private Boolean isFollower;
 
     public IsFollowerTask(AuthToken authToken, User follower, User followee, Handler messageHandler) {
-        this.authToken = authToken;
+        super(messageHandler, authToken);
         this.follower = follower;
         this.followee = followee;
-        this.messageHandler = messageHandler;
+    }
+
+    private Boolean checkIsFollower() {
+        return new Random().nextInt() > 0;
     }
 
     @Override
-    public void run() {
-        try {
-
-            sendSuccessMessage(new Random().nextInt() > 0);
-
-        } catch (Exception ex) {
-            Log.e(LOG_TAG, ex.getMessage(), ex);
-            sendExceptionMessage(ex);
-        }
+    protected void runTask() {
+        this.isFollower = checkIsFollower();
     }
 
-    private void sendSuccessMessage(boolean isFollower) {
-        Bundle msgBundle = new Bundle();
-        msgBundle.putBoolean(SUCCESS_KEY, true);
+    @Override
+    protected void loadSuccessBundle(Bundle msgBundle) {
         msgBundle.putBoolean(IS_FOLLOWER_KEY, isFollower);
-
-        Message msg = Message.obtain();
-        msg.setData(msgBundle);
-
-        messageHandler.sendMessage(msg);
-    }
-
-    private void sendFailedMessage(String message) {
-        Bundle msgBundle = new Bundle();
-        msgBundle.putBoolean(SUCCESS_KEY, false);
-        msgBundle.putString(MESSAGE_KEY, message);
-
-        Message msg = Message.obtain();
-        msg.setData(msgBundle);
-
-        messageHandler.sendMessage(msg);
-    }
-
-    private void sendExceptionMessage(Exception exception) {
-        Bundle msgBundle = new Bundle();
-        msgBundle.putBoolean(SUCCESS_KEY, false);
-        msgBundle.putSerializable(EXCEPTION_KEY, exception);
-
-        Message msg = Message.obtain();
-        msg.setData(msgBundle);
-
-        messageHandler.sendMessage(msg);
     }
 }
